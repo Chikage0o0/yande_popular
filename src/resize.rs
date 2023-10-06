0cd0b9878d951565e640c23f2441890325b69565
Chikage <chikage@939.me>
2023-10-07 01:08:32 +0900

    选择兼容性更佳的jpeg作为图片格式

--- src/resize.rs
+++ src/resize.rs
@@ -1,57 +1,29 @@
 use std::path::{Path, PathBuf};
 
 use anyhow::Result;
-use image::GenericImageView;
+use image_compressor::{
+    compressor::{Compressor, ResizeType},
+    Factor,
+};
 
-type Rgba = Vec<u8>;
-
-const MAX: u32 = 1920;
-
-fn get_scale(width: u32, height: u32) -> f32 {
-    if width < MAX && height < MAX {
-        return 1.0;
-    }
-    if width > height {
-        width as f32 / MAX as f32
-    } else {
-        height as f32 / MAX as f32
-    }
-}
-
-fn resize(path: &Path) -> Result<(Rgba, u32, u32)> {
-    let mut img = image::open(path)?;
-    let (width, height) = img.dimensions();
-    let scale = get_scale(width, height);
-    if scale > 1.0 {
-        img = img.resize(
-            (width as f32 / scale) as u32,
-            (height as f32 / scale) as u32,
-            image::imageops::FilterType::Lanczos3,
-        );
-    }
-    let rgba = img.to_rgba8().to_vec();
-
-    let (width, height) = img.dimensions();
-    dbg!(width, height);
-
-    Ok((rgba, width, height))
-}
-
-fn compress(rgba: &Rgba, width: u32, height: u32) -> Result<Vec<u8>> {
-    let encoder = webp::Encoder::from_rgba(rgba.as_slice(), width, height);
-    let webp = encoder.encode(85.0);
-    let webp = webp.to_vec();
-    Ok(webp)
-}
+const LIMIT: usize = 1920;
 
 pub fn resize_and_compress(path: &Path) -> Result<PathBuf> {
-    let (rgba, width, height) = resize(path)?;
-    let webp = compress(&rgba, width, height)?;
-    let mut new_path = path.to_path_buf();
-    new_path.set_extension("webp");
-    std::fs::write(&new_path, webp)?;
-    std::fs::remove_file(path)?;
-    Ok(new_path)
+    let source = path;
+    let dest = path.parent().unwrap_or(Path::new("."));
+    std::fs::create_dir_all(dest)?;
+    let mut comp = Compressor::new(source, &dest);
+    comp.set_factor(Factor::new_with_resize_type(
+        85.0,
+        ResizeType::LongestSidePixels(LIMIT),
+    ));
+    comp.set_delete_source(false);
+    comp.set_overwrite_dest(true);
+    let path = comp.compress_to_jpg().unwrap_or_else(|e| {
+        log::error!("compress failed: {}", e);
+        source.to_path_buf()
+    });
+    Ok(path)
 }
 
 #[cfg(test)]
@@ -63,8 +35,9 @@ mod test {
     fn test_resize_and_compress() -> Result<()> {
         env::set_var("RUST_LOG", "debug");
         env_logger::init();
-        let path = Path::new(r"D:\Project\yande_popular\data\tmp\1083075.jpg");
-        resize_and_compress(path)?;
+        let path = Path::new(r"D:\Project\yande_popular\data\tmp\Term.png");
+        let path = resize_and_compress(path)?;
+        log::debug!("path: {:?}", path);
         Ok(())
     }
 }
